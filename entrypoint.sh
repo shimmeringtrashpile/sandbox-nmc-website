083cda6994b651923b590930788b373cf75b978d
Rob Ray <47587146+shimmeringtrashpile@users.noreply.github.com>
2024-10-04 14:26:08 -0700

    Add clarifying comment.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# This script runs inside the container
+
+
 # abort if we reference a variable which has not been set.
 set -e -u
 
 # Double square brackets tests the condition
 # usermod exists on linux but not BSD/osx
+# usermod is returning UID '0' already exists on the server
 [[ $USERID ]] && usermod --uid "${USERID}" www-data
 
 exec "$@"
